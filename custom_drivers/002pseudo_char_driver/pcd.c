51cfaeb317398501452cee973f2a7fa9fd370678
KPiekorz <kpiekorz919@protonmail.com>
2021-11-01 22:43:17 +0100

    add error handling

--- custom_drivers/002pseudo_char_driver/pcd.c
+++ custom_drivers/002pseudo_char_driver/pcd.c
@@ -20,10 +20,38 @@ dev_t device_number;
 /* cdev variable */
 struct cdev pcd_cdev;
 
-loff_t pcd_lseek(struct file * filp, loff_t off, int whence)
+loff_t pcd_lseek(struct file * filp, loff_t offset, int whence)
 {
+	loff_t temp;
+
 	pr_info("lseek request\n");
-	return 0;
+	pr_info("current value of the file position = %lld\n", filp->f_pos);
+
+	switch(whence) {
+		case SEEK_SET:
+			if((offset > DEV_MEM_SIZE) || (offset < 0))
+				return -EINVAL;
+			filp->f_pos = offset;
+			break;
+		case SEEK_CUR:
+			temp = filp->f_pos + offset;
+			if((temp > DEV_MEM_SIZE) || (temp < 0))
+				return -EINVAL;
+			filp->f_pos = temp;
+			break;
+		case SEEK_END:
+			temp = DEV_MEM_SIZE + offset;
+			if((temp > DEV_MEM_SIZE) || (temp < 0))
+				return -EINVAL;
+			filp->f_pos = temp;
+			break;
+		default:
+			return -EINVAL;
+	}
+
+	pr_info("new value of the file position = %lld\n", filp->f_pos);
+	
+	return filp->f_pos;
 }
 
 ssize_t pcd_read(struct file * filp, char __user * buff, size_t count, loff_t * f_pos)
@@ -59,8 +87,10 @@ ssize_t pcd_write(struct file * filp, const char __user * buff, size_t count, lo
 	if((*f_pos + 	count) > DEV_MEM_SIZE)
 		count = DEV_MEM_SIZE - *f_pos;
 
-	if(!count)
+	if(!count) {
+		pr_err("No space left on the device\n");
 		return -ENOMEM;
+	}	
 
 	/* Copy to user */
 	if(copy_from_user(&device_buffer[*f_pos], buff, count)) {
@@ -85,7 +115,7 @@ int pcd_open(struct inode * inode, struct file * filp)
 
 int pcd_release(struct inode * inode, struct file * filp)
 {
-	pr_info("close successful\n");
+	pr_info("release successful\n");
 	return 0;
 }
 
@@ -111,8 +141,14 @@ struct device * device_pcd;
 
 static int __init pcd_driver_init(void)
 {
+	int ret;
+
 	/* 1. Dynamically allocate a device number */
-	alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+	ret = alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+	if(ret < 0) {
+		pr_err("Alloc chrdev failed\n");
+		goto out;
+	}
 
 	pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
@@ -121,17 +157,41 @@ static int __init pcd_driver_init(void)
 
 	/* 3. Register a device (cdev structure) with VFS */
 	pcd_cdev.owner = THIS_MODULE;
-	cdev_add(&pcd_cdev,  device_number, 1);
+	ret = cdev_add(&pcd_cdev,  device_number, 1);
+	if(ret < 0) {
+		pr_err("Cdev add failed\n");
+		goto unreg_chardev;
+	}
 
 	/* 4. Create device class under /sys/class/ */
 	class_pcd = class_create(THIS_MODULE, "pcd_class");
+	if(IS_ERR(class_pcd)) {
+		pr_err("Class creation failes\n");
+		ret = PTR_ERR(class_pcd);
+		goto cdev_del;
+	}
 
 	/* 5. populate the sysfs with device information */
 	device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
+	if(IS_ERR(device_pcd)) {
+		pr_err("Device create failed\n");
+		ret = PTR_ERR(device_pcd);
+		goto class_del;
+	}
 
 	pr_info("Module init was successful\n");
 
 	return 0;
+
+class_del:
+	class_destroy(class_pcd); 
+cdev_del:
+	cdev_del(&pcd_cdev);
+unreg_chardev:
+	unregister_chrdev_region(device_number, 1);	
+out:
+	pr_info("Module instertion failed\n");
+	return ret;
 }
 
 static void __exit pcd_driver_cleanup(void)
